22efdec5e829db5e34bc910fcecd36b54e5a4959
Rhett <rhetthay66@gmail.com>
2024-01-25 07:32:40 -0500

    Refactored raycasting to attack enemies.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -9,54 +9,51 @@ namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
-
         // Update is called once per frame
         void Update()
         {
-            InteractWithCombat();
-            InteractWithMovement();           
+            if(InteractWithCombat()) return;
+            if(InteractWithMovement()) return;
         }
 
-        private void InteractWithCombat()
+        private bool InteractWithCombat()
         {
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
             foreach (RaycastHit hit in hits) 
             {
                 CombatTarget target = hit.transform.GetComponent<CombatTarget>();
 
-                if (target != null) continue;
+                if (target == null) continue;
 
                 if (Input.GetMouseButtonDown(0))
                 {
                     GetComponent<Fighter>().Attack(target);
-                } 
+                }
+                return true;
             }
+            return false;
         }
 
-        private void InteractWithMovement()
-        {
-            if (Input.GetMouseButton(0))
+        private bool InteractWithMovement()
+        {         
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+
+            if (hasHit)
             {
-                MoveToCursor();
+                if (Input.GetMouseButton(0))
+                {
+                    GetComponent<Movement>().MoveTo(hit.point);
+                }
+                Debug.DrawRay(GetMouseRay().origin, GetMouseRay().direction * 100);
+                return true;
             }
+            return false;            
         }
 
         private static Ray GetMouseRay()
         {
             return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
-
-        private void MoveToCursor()
-        {
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
-
-            if (hasHit)
-            {
-                GetComponent<Movement>().MoveTo(hit.point);
-            }
-
-            Debug.DrawRay(GetMouseRay().origin, GetMouseRay().direction * 100);
-        }
-    }
+    }   
 }
