54c046d5dfca380fd97db7e157e916e56d5153fe
Rhett <rhetthay66@gmail.com>
2024-02-18 02:01:17 -0500

    Have enemies move along its patrol path at different speeds. Also, make enemies attack player at a quicker speed than their patrol speed.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -22,7 +22,7 @@ namespace RPG.Combat
 
             if (!GetIsInRange())
             {
-                GetComponent<Movement>().MoveTo(target.transform.position);
+                GetComponent<Movement>().MoveTo(target.transform.position, 1f);
             }
             else
             {
