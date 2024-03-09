535fc50f03b0832eba933e35bd4dbfbeea174f31
Rhett <rhetthay66@gmail.com>
2024-03-09 07:04:07 -0500

    Player can from one scene to another through a Portal.

---
+++ Assets/Scripts/SceneManagement/Portals.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+namespace RPG.SceneManagement
+{
+    public class Portals : MonoBehaviour
+    {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+        [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                StartCoroutine(PortalTransition());
+            }
+        }
+
+        private IEnumerator PortalTransition()
+        {
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set.");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portals otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject, 0.1f);
+        }
+
+        private void UpdatePlayer(Portals otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portals GetOtherPortal()
+        {
+            foreach (Portals portal in FindObjectsOfType<Portals>())
+            {
+                if (portal == this) continue;
+
+                if (portal.destination != destination) continue;
+                
+                return portal;
+            }
+
+            return null;
+        }
+    }
+}
+
